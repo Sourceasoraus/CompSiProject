d8b96bb8cc670ca8783ee368f06dbd0974bdd817
Sourceasoraus <githubformegan@gmail.com>
2025-04-30 20:10:25 -0500

    adding git info page

--- src/pages/GetInfo/GetInfo.tsx
+++ src/pages/GetInfo/GetInfo.tsx
@@ -1,13 +1,33 @@
 import styles from "./GetInfo.module.css";
+import FirstReps from "../../assets/FirstReps1.png";
+import SecReps from "../../assets/SecReps.png";
+import ThirdReps from "../../assets/ThirdReps.png";
+import FourthReps from "../../assets/FourthReps.png";
 
 function GetInfo() {
   return (
     <div className={styles.container}>
       <h1>Get Info Page</h1>
-      <p>
-        This page will eventually provide civic information. Placeholder for
-        now.
-      </p>
+      <img
+        src={FirstReps}
+        alt="First Representatives Screenshot"
+        className={styles.image}
+      />
+      <img
+        src={SecReps}
+        alt="Second Representatives Screenshot"
+        className={styles.image}
+      />
+      <img
+        src={ThirdReps}
+        alt="Third Representatives Screenshot"
+        className={styles.image}
+      />
+      <img
+        src={FourthReps}
+        alt="Fourth Representatives Screenshot"
+        className={styles.image}
+      />
     </div>
   );
 }
